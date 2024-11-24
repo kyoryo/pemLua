afe54130b0a51f7b8940a89c0d2a445a5055c4da
kyoryo <franz.rio19@gmail.com>
2024-11-24 15:40:28 +0700

    commit mantapp

--- introduction/hw.lua
+++ introduction/hw.lua
@@ -2,4 +2,6 @@
 print("hello world!")
 
 -- ini komitan dari main
--- ini conflict dari coba-git-merge
+-- ini conflict dari coba-git-merge tapi diubah di coba-git-merge
+--
+-- tambahan dari coba-git-merge
